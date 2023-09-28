9e0b308360cbf186e85a65a8b754d128e751eb88
Michael Akerele <98211469+aces563@users.noreply.github.com>
2023-09-28 23:41:43 +0300

    add templates

---
+++ themes/contrib/stacknatic/js/build/scripts.js
@@ -0,0 +1,2 @@
+"use strict";!function(n,o){n.behaviors.stacknatic={attach:function(n,i){o(window).on("load",function(){}),o(window).on("resize",function(){}),o(window).on("scroll",function(){})}}}(Drupal,jQuery);
+//# sourceMappingURL=scripts.js.map
