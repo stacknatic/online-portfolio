8a1d252b893119ccd734aa5f9fee8ad4b19b01d3
Michael Akerele <98211469+aces563@users.noreply.github.com>
2023-10-05 17:24:26 +0300

    add chat modal

--- themes/contrib/portfolio_two/js/main.js
+++ themes/contrib/portfolio_two/js/main.js
@@ -1,5 +1,44 @@
-console.log('Hello Drupal!');
+console.log('Hello today Drupal!');
 
+// const startLiveChatForm = document.querySelector('.start-live-chat-modal');
+// const declineLiveChat = document.querySelector('.close-live-chat-form');
+
+// if(!localStorage.getItem('LiveChatStacknatic')) {
+
+//     setTimeout(() => {
+//         startLiveChatForm.classList.add('active');
+//     }, 10000);
+
+// }
+// declineLiveChat.addEventListener('click', () => {
+//     startLiveChatForm.classList.remove('active');
+//     // localStorage.setItem('LiveChatStacknatic', 'respondedLiveChat');
+// });
+
+// document.querySelector('#room-name-input').focus();
+// document.querySelector('#room-name-input').onkeyup = function(e) {
+//     if (e.keyCode === 13) {  // enter, return
+//         document.querySelector('#room-name-submit').click();
+//     }
+// };
+
+// document.querySelector('#room-name-submit').onclick = function(e) {
+//     const visitorName = document.querySelector('#room-name-input').value;
+//     const cleanedName = cleanInput(visitorName)
+//     // const addCharacters = randomCharacters(12)
+//     randomCharacters(30)
+//     .then(addCharacters => {
+//         window.location.pathname = '/chat/' + cleanedName + addCharacters + '/';
+//         // localStorage.setItem('LiveChatStacknatic', 'respondedLiveChat');
+//     })
+//     .catch(error => {
+//         alert('Unable to initiate a live chat from your browser. You can try to update your browser and try again or try with another browser');
+//     });
+// };
+
+// fetch
+
+// book chatroom with fetch
 const startLiveChatForm = document.querySelector('.start-live-chat-modal');
 const declineLiveChat = document.querySelector('.close-live-chat-form');
 
@@ -7,12 +46,12 @@ if(!localStorage.getItem('LiveChatStacknatic')) {
 
     setTimeout(() => {
         startLiveChatForm.classList.add('active');
-    }, 10000);
+    }, 3000);
 
 }
 declineLiveChat.addEventListener('click', () => {
     startLiveChatForm.classList.remove('active');
-    // localStorage.setItem('LiveChatStacknatic', 'respondedLiveChat');
+    localStorage.setItem('LiveChatStacknatic', 'respondedLiveChat');
 });
 
 document.querySelector('#room-name-input').focus();
@@ -22,19 +61,117 @@ document.querySelector('#room-name-input').onkeyup = function(e) {
     }
 };
 
-document.querySelector('#room-name-submit').onclick = function(e) {
-    const visitorName = document.querySelector('#room-name-input').value;
+document.querySelector('#room-name-submit').addEventListener('click', async function(e) {
+    e.preventDefault();
+    const visitorName = document.querySelector('#room-name-input').value.trim();
     const cleanedName = cleanInput(visitorName)
-    // const addCharacters = randomCharacters(12)
-    randomCharacters(30)
-    .then(addCharacters => {
-        window.location.pathname = '/chat/' + cleanedName + addCharacters + '/';
-        // localStorage.setItem('LiveChatStacknatic', 'respondedLiveChat');
+
+    
+    if (visitorName.length < 1) {
+        alert('Please enter your name');
+        return;
+    }
+
+    const randomC = await randomCharacters(30);
+    
+    createRoom(cleanedName, randomC);
+});
+
+function createRoom(cleanedName, randomC) {
+    const chatModal = document.querySelector('.chat-modal-container');
+    fetch('http://localhost:8000/sign/chat-invitation/', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'X-Requested-With': 'AJAX', // custom header
+        },
+        body: JSON.stringify({
+            "invitee": cleanedName,
+            "randomC": randomC
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.invitation_key) {
+            localStorage.setItem('room-name', randomC);
+            chatModal.classList.add('active');
+            startLiveChatForm.classList.remove('active');
+            // window.location.pathname = '/chat/' + randomC + '/';
+        } else if (data.feedback) {
+            alert(data.feedback);
+        }
     })
     .catch(error => {
         alert('Unable to initiate a live chat from your browser. You can try to update your browser and try again or try with another browser');
     });
-};
+}
+
+// const startLiveChatForm = document.querySelector('.start-live-chat-modal');
+// const declineLiveChat = document.querySelector('.close-live-chat-form');
+
+// if(!localStorage.getItem('LiveChatStacknatic')) {
+
+//     setTimeout(() => {
+//         startLiveChatForm.classList.add('active');
+//     }, 3000);
+
+// }
+// declineLiveChat.addEventListener('click', () => {
+//     startLiveChatForm.classList.remove('active');
+//     localStorage.setItem('LiveChatStacknatic', 'respondedLiveChat');
+// });
+
+// document.querySelector('#room-name-input').focus();
+// document.querySelector('#room-name-input').onkeyup = function(e) {
+//     if (e.keyCode === 13) {  // enter, return
+//         document.querySelector('#room-name-submit').click();
+//     }
+// };
+
+
+// document.querySelector('#room-name-submit').addEventListener('click', async function(e) {
+//     e.preventDefault();
+//     const visitorName = document.querySelector('#room-name-input').value.trim();
+//     const cleanedName = cleanInput(visitorName)
+
+    
+//     if (visitorName.length < 1) {
+//         alert('Please enter your name');
+//         return;
+//     }
+
+//     const randomC = await randomCharacters(30);
+    
+//     createRoom(cleanedName, randomC);
+// });
+
+// function createRoom(cleanedName, randomC) {
+//     fetch('http://lawtech.com/sign/chat-invitation/', {
+//         method: 'POST',
+//         headers: {
+//             'Accept': 'application/json',
+//             'X-Requested-With': 'AJAX', // custom header
+//         },
+//         body: JSON.stringify({
+//             "invitee": cleanedName,
+//             "randomC": randomC
+//         })
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         if (data.invitation_key) {
+//             localStorage.setItem('LiveChatStacknatic', 'reacted');
+//             window.location.href = 'http://lawtech.com/chat/' + randomC + '/';
+//         } else if (data.feedback) {
+//             alert(data.feedback);
+//         }
+//     })
+//     .catch(error => {
+//         alert('Unable to initiate a live chat from your browser. You can try to update your browser and try again or try with another browser');
+//     });
+// }
+
+//
 
 //sanitizes the input
 function cleanInput(inputString) {
@@ -93,4 +230,4 @@ const navSlide= () => {
 } 
 burger.addEventListener('click', navSlide)
 
-  
+
